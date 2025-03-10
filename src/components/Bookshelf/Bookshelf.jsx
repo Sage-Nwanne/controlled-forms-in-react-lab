41bf271834c9ed8bca36fd167ef536591ec5eab9
User Name <user@email.com>
2025-03-10 02:48:52 -0400

    added state variables, handler functions, connected function to user input, added map function for books added to bookshelf... Lab complete

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+const Bookshelf = () => {
+
+const [books, setBooks] = useState ([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+])
+
+const [newBook, setNewBook] = useState ([{
+    title: '',
+    author: '',
+}])
+
+
+const handleInputChange = (event) => {
+    setNewBook({ ...newBook, [event.target.name] : event.target.value});
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBooks({...books, [event.target.name] : [event.target.value]});
+        setNewBook({
+            title: '',
+            author: ''
+     })
+    }
+    
+
+
+
+
+
+
+    return (
+<div className="bookshelfDiv">
+  <div className="formDiv">
+    <h3>Add a Book</h3>
+    <form onSubmit={handleSubmit}>
+
+<label htmlFor="title">Title: </label>
+<input id="title" name="title" value={newBook.title} onChange={handleInputChange}></input>
+
+<label htmlFor="author">Author: </label>
+<input id="author" name="author" value={newBook.author} onChange={handleInputChange}></input>
+<button onSubmit={handleSubmit}>Submit</button>
+    </form>
+  </div>
+  <div className="bookCardsDiv">
+    <h3>Your Bookshelf</h3>
+    {books.map((book, index) => (
+        <div key={index} className="bookCard">
+            <h4>Title: {book.title} </h4>
+            <h4>Author: {book.author} </h4>
+        </div>
+))}
+</div>
+</div>
+ );
+};
+
+
+export default Bookshelf;
